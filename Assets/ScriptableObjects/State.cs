50034e1ecca09f07df138c0786b3b3f4c034d025
leopokoyo <leopokoyo@hotmail.com>
2023-11-18 20:47:36 +0100

    State Machine Abstract Classes
    Added the StateRunner Abstract Class
    Added the State Abstract Class

---
+++ Assets/ScriptableObjects/State.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+namespace ScriptableObjects
+{
+    public abstract class State<T> : ScriptableObject where T : MonoBehaviour
+    {
+        protected T Parent;
+
+        public virtual void Init(T parent)
+        {
+            Parent = parent;
+        }
+
+        public abstract void ChangeState();
+        public abstract void ExitState();
+        public abstract void Update();
+        public abstract void HandleInput();
+    }
+}
---
+++ Assets/ScriptableObjects/StateRunner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+namespace ScriptableObjects
+{
+    public abstract class StateRunner<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        
+        [Header("Different States")]
+        [SerializeField] private List<State<T>> states;
+
+
+        private State<T> _activeState;
+        private readonly Dictionary<Type, State<T>> _stateByType = new();
+
+        protected void Awake()
+        {
+            states.ForEach(state => _stateByType.Add(state.GetType(), state));
+        }
+
+        public void SetState(Type newStateType)
+        {
+            if (_activeState != null)
+            {
+                _activeState.ExitState();
+                
+            }
+            _activeState = states.First(state => state.GetType() == newStateType);
+            _activeState.Init(GetComponent<T>());
+        }
+
+        private void Update()
+        {
+            _activeState.HandleInput();
+            _activeState.Update();
+            _activeState.ChangeState();
+        }
+    }
+}
