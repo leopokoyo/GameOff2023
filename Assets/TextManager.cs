d250e4b4fe1a47a1717f86722ea8c1de537ad810
leopokoyo <leopokoyo@hotmail.com>
2023-11-27 20:51:52 +0100

    TextManager Fixed
    TextManager Fixed

--- Assets/TextManager.cs
+++ Assets/TextManager.cs
@@ -6,7 +6,7 @@ using TMPro;
 
 public class TextManager : MonoBehaviour
 {
-    PlanetData[] planets;
+    WorldController[] planets;
     public TextMeshProUGUI standardTextBox;
     TextMeshProUGUI[] textBox;
     
@@ -19,20 +19,7 @@ public class TextManager : MonoBehaviour
     void Start()
     {
 
-        planets = GetComponentsInChildren<PlanetData>();
-
-        foreach (PlanetData planet in planets)
-        {
-            List<Goods> keys = new List<Goods>(planet.inventory.Keys);
-            foreach (Goods item in keys)
-            {
-                {
-                    TextMeshProUGUI textBox = Instantiate(standardTextBox, planet.planetLocation, transform.rotation);
-                    textBox.text = item.ToString() + " Stockpile: " + planet.inventory[item] + " Price: " + planet.prices[item];
-
         planets = GetComponentsInChildren<WorldController>();
-        
-        
 
         var j = 0;
 
