ab6217c6b0ecb729b4003b498fe357882a8ab5f9
leopokoyo <leopokoyo@hotmail.com>
2023-11-30 17:23:01 +0100

    Change Adui stuff
    asdfasdf

--- Assets/Scripts/Audio/FmodEvents.cs
+++ Assets/Scripts/Audio/FmodEvents.cs
@@ -1,4 +1,31 @@
-public class FmodEvents
+using System;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+public class FmodEvents : MonoBehaviour
 {
     
-}
+    public static FmodEvents Instance { get; private set;}
+    
+    [field: Header("Music")]
+    [field: SerializeField]
+    public EventReference Music { get; private set;}
+    
+    
+    [field: Header("Ambience")]
+    [field: SerializeField]
+    public EventReference Ambience { get; private set;}
+
+    private void Awake()
+    {
+        if (Instance != this && Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;    
+        }
+    }
+} 
