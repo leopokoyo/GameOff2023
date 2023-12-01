bbb661e10cd88cb7ada8ce2a9b8a355c52013ce9
leopokoyo <leopokoyo@hotmail.com>
2023-12-01 15:41:17 +0100

    ShipOptionsFixed

--- Assets/ShipOptionText.cs
+++ Assets/ShipOptionText.cs
@@ -1,12 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
-using TMPro;
-=======
 using System.Diagnostics.Contracts;
 using TMPro;
 using Unity.VisualScripting;
->>>>>>> BartsThirdBranch
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,20 +11,14 @@ public class ShipOptionText : MonoBehaviour
     public ShipController ship;
     public WorldController[] planets = new WorldController[3];
 
-<<<<<<< HEAD
-=======
     WorldController currentWorld;
 
->>>>>>> BartsThirdBranch
     public TMP_Dropdown planetDropdown;
     public TMP_Dropdown goodDropdown;
     public TMP_Dropdown buySellDropdown;
     public TMP_InputField amountInput;
-<<<<<<< HEAD
-=======
 
     List<ICommand> commands = new List<ICommand>();
->>>>>>> BartsThirdBranch
     
     Vector3 planetLocation = new Vector3(0,0,0);
     Goods typeOfGood;
@@ -40,18 +30,6 @@ public class ShipOptionText : MonoBehaviour
     public void HandlePlanetInput(int val)
     {
         val = planetDropdown.value;
-<<<<<<< HEAD
-        if (val == 0){
-            planetLocation = planets[1].transform.position;
-        }
-
-        if (val == 1){
-            planetLocation = planets[2].transform.position;
-        }
-
-        if (val == 2){
-            planetLocation = planets[3].transform.position;
-=======
         if (val == 1){
             planetLocation = planets[0].planetData.planetLocation;
             currentWorld = planets[0];
@@ -65,32 +43,12 @@ public class ShipOptionText : MonoBehaviour
         if (val == 3){
             planetLocation = planets[2].planetData.planetLocation;
             currentWorld = planets[2];
->>>>>>> BartsThirdBranch
         }
     }
 
     public void HandleTradeGoodInput(int val)
     {
         val = goodDropdown.value;
-<<<<<<< HEAD
-        if (val == 0){
-            typeOfGood = Goods.steelWool;
-        }
-
-        if (val == 1){
-            typeOfGood = Goods.goop;
-        }
-
-        if (val == 2){
-            typeOfGood = Goods.dogToys;
-        }
-
-        if (val == 3){
-            typeOfGood = Goods.cheese;
-        }
-
-        if (val == 4){
-=======
         if (val == 1){
             typeOfGood = Goods.steelWool;
         }
@@ -108,7 +66,6 @@ public class ShipOptionText : MonoBehaviour
         }
 
         if (val == 5){
->>>>>>> BartsThirdBranch
             typeOfGood = Goods.water;
         }
     }
@@ -122,28 +79,17 @@ public class ShipOptionText : MonoBehaviour
     public void SetBuyOrSell(int val){
         val = buySellDropdown.value;
 
-<<<<<<< HEAD
-        if (val == 0){
-            isBuying = true;
-        }
-
-        if (val == 1){
-=======
         if (val == 1){
             isBuying = true;
         }
 
         if (val == 2){
->>>>>>> BartsThirdBranch
             isBuying = false;
         }
     }
 
     public void GiveInfo()
     {
-<<<<<<< HEAD
-        Debug.Log(planetLocation + " " + typeOfGood + " " + amountOfGoods + " " + isBuying);
-=======
         
         if (isBuying){
             commands.Add(new BuyCommand(ship, currentWorld, typeOfGood, amountOfGoods));
@@ -159,6 +105,5 @@ public class ShipOptionText : MonoBehaviour
         ship.NewContract(newContract);
 
         commands.Remove(commands[0]);
->>>>>>> BartsThirdBranch
     }
 }
